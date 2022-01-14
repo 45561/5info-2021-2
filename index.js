f26f97e5ee32b974d096d3b968c85a99f7411e25
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-01-14 15:53:34 -0300

    Merge branch 'master' of

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.post("/", function (req, res) {
   res.send("Resposta pelo metodo POST");
 });
 
-app.get("/:nome/:sobrenome", function (req, res) {
-  res.send("Oi " + req.params.nome + " " + req.params.sobrenome);
-});
-
 app.listen("3000", function () {
   console.log("Conexao iniciada!");
 });
