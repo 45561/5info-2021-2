33ffa2e0842d7ab682cfe2f7b7fe934f1e8fa13c
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-01-14 15:59:56 -0300

    alteração de rotas

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ app.get("/", function (req, res) {
   res.send("Resposta pelo metodo GET");
 });
 
-app.post("/", function (req, res) {
-  console.log("Executando metodo POST");
-  res.send("Resposta pelo metodo POST");
-});
-
 app.listen("3000", function () {
   console.log("Conexao iniciada!");
 });
