fdfefeb31fbd81cd85d155e8cccc3e093c2cdc9e
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-01-14 15:47:29 -0300

    Alteração nas rotas do Index.js

--- index.js
+++ index.js
@@ -2,18 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  //console.log(req);
-  //let soma = parseInt(req.query.num1) + parseInt(req.query.num2);
   res.send("Resposta pelo metodo GET");
-});
-
-app.post("/", function (req, res) {
-  console.log("Executando metodo POST");
-  res.send("Resposta pelo metodo POST");
-});
-
-app.get("/:nome/:sobrenome", function (req, res) {
-  res.send("Oi " + req.params.nome + " " + req.params.sobrenome);
 });
 
 app.listen("3000", function () {
